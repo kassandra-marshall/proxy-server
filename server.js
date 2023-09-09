1745ac766f509c485937526866359439a34601aa
dyzn3y <kdmarshall728@gmail.com>
2023-09-09 09:19:07 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+const API_URL = 'https://www.googleapis.com/books/v1/volumes' 
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/api', (req, res) => {
+    console.log('get request')
+  request(
+    { url: `${API_URL}` },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: error.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  );
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
